8e3fe5dca0735977aa7cef23ef783ee8d7412242
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-01 18:45:25 +0530

    Add functionality to greet user

--- jarvis.py
+++ jarvis.py
@@ -1,12 +1,32 @@
 import pyttsx3
+import datetime
+import SpeechRecognition as sr
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
+
 def speak(audio):
+    """Takes a string and outputs it through default speakers"""
     engine.say(audio)
-    engine.runAndWait()    
+    engine.runAndWait()
+
+
+def wishMe():
+    """Speaks the greeting based on the current system time"""
+
+    hour = int(datetime.datetime.now().hour)
+    if hour >= 4 and hour < 12:
+        speak('Good Morning Aditya Sir!')
+    elif hour >= 12 and hour < 17:
+        speak('Good Afternoon Aditya Sir!')
+    elif hour >= 17 and hour < 22:
+        speak('Good Evening Aditya Sir!')
+    else:
+        speak('Hello Aditya Sir!')
+    speak('I am Jarvis. I am here to help you')
+
 
 if __name__ == "__main__":
-    speak("I hope it is working")
+    wishMe()
