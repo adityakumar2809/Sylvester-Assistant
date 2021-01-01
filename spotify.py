36289a03ffba5f9419cc9e32dae09a1f7a2d15e6
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-01 19:44:45 +0530

    Add on Spotify Module

---
+++ spotify.py
@@ -0,0 +1,23 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+from decouple import config
+
+SPOTIPY_CLIENT_ID = config('SPOTIPY_CLIENT_ID')
+SPOTIPY_CLIENT_SECRET = config('SPOTIPY_CLIENT_SECRET')
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify(
+    client_credentials_manager=SpotifyClientCredentials(
+        SPOTIPY_CLIENT_ID,
+        SPOTIPY_CLIENT_SECRET
+    ))
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
