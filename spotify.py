7f6c363212ee06cc73d7e6f7d7a02f03c5db11e3
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-01 20:26:00 +0530

    Modify Scope and get Device ID

--- spotify.py
+++ spotify.py
@@ -2,6 +2,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 
 from decouple import config
+import json
 
 SPOTIPY_CLIENT_ID = config('SPOTIPY_CLIENT_ID')
 SPOTIPY_CLIENT_SECRET = config('SPOTIPY_CLIENT_SECRET')
@@ -11,7 +12,10 @@ scope = [
     'user-library-read', 
     'user-read-recently-played', 
     'user-top-read', 
-    'user-read-playback-position'
+    'user-read-playback-position',
+    'user-read-private',
+    'user-read-playback-state',
+    'user-modify-playback-state'
 ]
 scope = ' '.join(map(str, scope)) 
 
@@ -24,4 +28,8 @@ sp = spotipy.Spotify(
         )
     )
 
-print(sp.current_user_recently_played(limit=1))
+# recently_played = sp.current_user_recently_played(limit=5)
+# print(json.dumps(recently_played, sort_keys=True, indent=4))
+
+devices = sp.devices()
+print(json.dumps(devices, sort_keys=True, indent=4))
