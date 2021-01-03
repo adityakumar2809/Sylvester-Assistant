399324c1452a40d6a1b13efe6b9fa12cfba4f842
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-03 20:37:17 +0530

    Make a clean joke function

--- my_jokes.py
+++ my_jokes.py
@@ -2,14 +2,12 @@ import requests
 import json
 
 
-def jokes(f):
-    
-    data = requests.get(f)
-    tt = json.loads(data.text)
-    return tt
+def get_random_joke():
+    url = r'https://official-joke-api.appspot.com/random_joke'
+    joke_response = requests.get(url)
+    joke_content = json.loads(joke_response.text)
+    return joke_content
 
-f = r"https://official-joke-api.appspot.com/random_joke"
-a = jokes(f)
 
-print(a["setup"])
-print(a["punchline"], "\n")
+if __name__ == "__main__":
+    get_random_joke()
