824b8e2e068f87747e1df80b132df8aedfdb1890
Aditya Kumar Singh <adityakumar2809@gmail.com>
2021-01-03 21:29:29 +0530

    Add feature to open the note file in notepad

--- my_notes.py
+++ my_notes.py
@@ -1,7 +1,13 @@
+import os
+
 def add_note(note):
     f = open('my_note_doc.txt', 'a')
     f.write(f'{note}\n\n')
     f.close()
 
+def open_note():
+    os.startfile('my_note_doc.txt')
+
 if __name__ == "__main__":
     add_note('This is an amazing piece of text')
+    open_note()
